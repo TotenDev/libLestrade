e8a3c92596cbba29d09d1876b75164629e134228
Marcos Trovilho <marcos@trovilho.com>
2012-10-18 15:06:38 -0300

    Initial Commit

---
+++ Lestrade/Lestrade.h
@@ -0,0 +1,13 @@
+//
+//  Lestrade.h
+//  Lestrade
+//
+//  Created by Marcos Trovilho on 10/18/12.
+//  Copyright (c) 2012 TotenDev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Lestrade : NSObject
+
+@end
---
+++ Lestrade/Lestrade.m
@@ -0,0 +1,13 @@
+//
+//  Lestrade.m
+//  Lestrade
+//
+//  Created by Marcos Trovilho on 10/18/12.
+//  Copyright (c) 2012 TotenDev. All rights reserved.
+//
+
+#import "Lestrade.h"
+
+@implementation Lestrade
+
+@end
